1f3ac0747f92186be8148aa0576b9832d484d384
lanzarrocas <56543649+lanzarrocas@users.noreply.github.com>
2020-05-08 19:48:36 +0200

    (1)
    Servidor compacto

--- P1/server.js
+++ P1/server.js
@@ -1,33 +1,13 @@
-//-- Puerto donde recibir las peticiones
-const PUERTO = 8080;
-
-//-- Modulo http
 const http = require('http');
+const PUERTO = 8080
 
 console.log("Arrancando servidor...")
 
-//-- Funcion para atender a una Peticion
-//-- req: Mensaje de solicitud
-//-- res: Mensaje de respuesta
-function peticion(req, res) {
+//-- Configurar el servidor. Cada vez que llegue una peteicion
+//-- se notifica en la consola
+server = http.createServer( (req, res) => {
+  console.log("---> Peticion recibida")
+}).listen(PUERTO);
 
-  //-- Peticion recibida
-  console.log("Peticion recibida!")
 
-  //-- Crear mensaje de respuesta
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Hello World!');
-
-}
-
-//-- Inicializar el servidor
-//-- Cada vez que recibe una petición
-//-- invoca a la funcion peticion para atenderla
-const server = http.createServer(peticion)
-
-//-- Configurar el servidor para escuchar en el
-//-- puerto establecido
-server.listen(PUERTO);
-
-console.log("Servidor LISTO!")
-console.log("Escuchando en puerto: " + PUERTO)
+console.log("Puerto: " + PUERTO)
