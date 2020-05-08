5fd87ca4f8e9d64a398c5da095d646283597b6cd
lanzarrocas <56543649+lanzarrocas@users.noreply.github.com>
2020-05-09 01:59:10 +0200

    (2)
    Servidor analiza la url

--- P1/server.js
+++ P1/server.js
@@ -1,5 +1,8 @@
 const http = require('http');
 const PUERTO = 8080
+const url = require('url');
+// -- API for interacting with the file system
+const fs = require('fs');
 
 console.log("Arrancando servidor...")
 
@@ -7,6 +10,21 @@ console.log("Arrancando servidor...")
 //-- se notifica en la consola
 server = http.createServer( (req, res) => {
   console.log("---> Peticion recibida")
+  console.log("Recurso solicitado (URL): " + req.url)
+
+  //-- Analisis de la URL recibida:
+  let q = url.parse(req.url, true);
+
+  console.log("Pathname: " +  q.pathname)
+  console.log("search: " + q.search)
+  console.log("Búsqueda:")
+  let qdata = q.query
+  console.log(qdata)
+
+  //-- Acceso al objeto
+  console.log("Artículo: " + qdata.articulo)
+  console.log("Color: " + qdata.color)
+
 }).listen(PUERTO);
 
 
