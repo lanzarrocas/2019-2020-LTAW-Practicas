d94d7606c0b459a04ea5532ecff0e6c189e776ac
lanzarrocas <56543649+lanzarrocas@users.noreply.github.com>
2020-05-11 05:59:06 +0200

    (4)
    He remodelado los html. Añadido botón de inicio, y de carrito de la compra.
    Por otra parte el servidor manda una cookie al navegador cuando este hace una petición del path (ruta) /login
    Modificaciones en css para dar formato al contenido añadido al html.

--- P1/server.js
+++ P1/server.js
@@ -4,45 +4,53 @@ const fs = require('fs');
 var path = require('path');
 const PUERTO = 8080
 
+// -- Se definen los tipos mime de nuestros ficheros
 var mimeTypes = {
   '.js' : 'text/javascript',
   '.html' : 'text/html',
   '.css' : 'text/css',
   '.png' : 'image/png'
 }
+
 //-- Configurar y lanzar el servidor. Por cada peticion recibida
 //-- se imprime un mensaje en la consola
 http.createServer((req, res) => {
-  console.log("----------> Peticion recibida")
+  console.log("----------> Peticion recibida (request from browser)")
   var buscar = path.basename(decodeURI(req.url)) || 'index.html' ,
   filename = 'content/' + buscar;
   console.log("Se ha solicitado el recurso " + filename );
+  console.log("buscar=" + buscar);
+  if (buscar == "login") {
+    res.setHeader('Set-Cookie', 'user=pablo')
+  }
   //-- Leer fichero
   fs.exists(filename, function(exists) {
+    // -- Si existe lo servimos
     if (exists) {
+
       fs.readFile(filename, function(err, data) {
             //-- Fichero no encontrado. Devolver mensaje de error
             if (err) {
               console.log("Se ha producido un error interno");
               res.writeHead(404, {'Content-Type': 'text/html'});
               return res.end("error del servidor");
-
             }
+            console.log("----------> Peticion respondida (response from server)")
             console.log("Recurso" + filename + "ENVIADO");
             console.log();
             var headers = {'Content-Type': mimeTypes[path.extname(buscar)]};
+
             //-- Generar el mensaje de respuesta
             res.writeHead(200, headers);
             //res.write(data);
             res.end(data);
-        });
+      });
         return;
-      }
-
+    }
+      // -- Si no existe devolvemos pagina no encontrada
       res.writeHead(404); // -- Archivo no encontrado
       res.end("pagina no encontrada");
-    });
-
+  });
 }).listen(PUERTO);
 
 console.log("Servidor corriendo...");
