e99081241c10ba62c71f1bf5058e3d3f756b91e3
lanzarrocas <56543649+lanzarrocas@users.noreply.github.com>
2020-05-13 01:40:06 +0200

    (5)
    organización de los recursos en carpetas. Puesta a punto del mecanismo de la web. Falta añadir formulario para introducir datos de compra y generar cookies al seleccionar los productos

--- P1/server.js
+++ P1/server.js
@@ -16,13 +16,48 @@ var mimeTypes = {
 //-- se imprime un mensaje en la consola
 http.createServer((req, res) => {
   console.log("----------> Peticion recibida (request from browser)")
+  // -- Guardo en buscar el recurso solicitado. Si no se accede a ninguno
+  // la variable guarda el valor 'index.html' m que corresponde a la página de inicio
   var buscar = path.basename(decodeURI(req.url)) || 'index.html' ,
-  filename = 'content/' + buscar;
+  // -- Guardo el formato del recurso solicitado ya que he creado
+  // diferentes carpetas para organizarlos
+  split = buscar.split(".")[1];
+    switch (split) {
+      case "html":
+        var filename = 'content/html/' + buscar;
+        break;
+      case "png":
+          var filename = 'content/png/' + buscar;
+          break;
+      case "css":
+          var filename = 'content/css/' + buscar;
+            break;
+      default:
+        var filename = 'content/' + buscar;
+          break;
+    }
+
+  // var filename = 'content/' + buscar;
   console.log("Se ha solicitado el recurso " + filename );
   console.log("buscar=" + buscar);
-  if (buscar == "login") {
+  console.log("split=" + split);
+  // -- Almaceno en una variable las cookies enviadas en
+  // la solicitud http del cliente
+  var cookie = req.headers.cookie;
+
+  // -- Si accedemos al login , se creará una cookie de registro
+  // que el servidor envia al cliente en el mensaje de respuesta
+  // el cliente almacenará esta cookie
+  if (buscar == "login.html") {
     res.setHeader('Set-Cookie', 'user=pablo')
+    console.log("Cookie creada y enviada al servidor: (" + cookie + ")");
   }
+  if (buscar == "carrito.html") {
+    if(!cookie) {
+      filename = 'content/noregistrado.html';
+    }
+ }
+
   //-- Leer fichero
   fs.exists(filename, function(exists) {
     // -- Si existe lo servimos
