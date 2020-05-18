acdb6da42b5c7b6eab55c9a60d9bcb8228fe295d
lanzarrocas <56543649+lanzarrocas@users.noreply.github.com>
2020-05-18 05:29:03 +0200

    (2)

--- P2/mi_proyectoweb/mi_proyectoweb/urls.py
+++ P2/mi_proyectoweb/mi_proyectoweb/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+
+# -- Recursos de mi proyecto web
 
 urlpatterns = [
+    # -- Esta es la URL de mi tienda
+    path('mi_tienda/', include('mi_tienda.urls')),
+
+    # -- URL de administracion (por defecto en django)
     path('admin/', admin.site.urls),
 ]
---
+++ P2/mi_proyectoweb/mi_tienda/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+
+# -- Importar todas las vistas de mi_tienda
+from . import views
+
+# -- Aquí se definen las URLs de nuestra tienda
+# -- Metemos de momento sólo la principal (índice)
+
+urlpatterns = [
+    # -- Vista pricipal (índice)
+    path('', views.index, name='index'),
+    path('/static/index.html', views.index2, name='index2'),
+    path('test1/', views.test1, name='test1'),
+    path('test2/', views.test2, name='test2'),
+    path('test3/', views.test3, name='test3'),
+    path('test4/', views.test4, name='test4'),
+    path('test5/', views.test5, name='test5'),
+
+
+]
